18c95f38532ad5580a99d0a14437e1e1643670dd
Byron Dowling <byrondowling7@gmail.com>
2023-03-11 15:31:08 -0600

    Added Code for making the sprites move and Jump

--- Python Gaming/Art Of War/ArtOfWar.py
+++ Python Gaming/Art Of War/ArtOfWar.py
@@ -130,6 +130,8 @@ class GameSprite:
 
 ## Initialize Pygame
 pygame.init()
+pygame.font.init()
+
 running = True
 
 ## Rough Dimensions of Byron's Monitor
@@ -144,7 +146,12 @@ screen = pygame.display.set_mode(size)
 # BackGround = Background("Arena.jpg", [0, 0])
 BackGround = Background("Arena_Night.jpg", [0, 0])
 
-## Pseudo-random character selection
+
+"""
+    Pseudo-random player selection, may eventually switch to player selection screen
+
+        Grabs the # of frames for idle movement and the name of the sprite
+"""
 C4 = PlayerSelector()
 sprites = C4.chooseSprites()
 P1 = sprites[0]
@@ -160,6 +167,35 @@ P2_name = P2["Screen Name"]
 
 pp = pprint.PrettyPrinter(depth=4)
 pp.pprint(sprites)
+
+## Get System Font Info
+# pp.pprint(pygame.font.get_fonts())
+
+
+## Default Player Variables to get ball rolling
+Player1_StartingPosition = (350,500)
+Player2_StartingPosition = (1350,500)
+Default_Smoothscale_Dimensions = (250,250)
+
+## Movement Variables
+PLAYER_SPEED = 10
+VERTICAL_SPEED = 15
+JUMP_HEIGHT = 150
+
+player1_x = Player1_StartingPosition[0]
+player1_y = Player1_StartingPosition[1]
+player2_x = Player2_StartingPosition[0]
+player2_y = Player2_StartingPosition[1]
+
+P1_Standing = True
+P1_Jumping = False
+P1_Descending = False
+P1_Jump_Height = 0
+
+P2_Standing = True
+P2_Jumping = False
+P2_Descending = False
+P2_Jump_Height = 0
 
 
 ## Set the title of the window
@@ -187,12 +223,50 @@ while running:
             running = False
             break
 
+    """
+        Player Sprite Movement shit
+    """
+    # Get the pressed keys
+    keys = pygame.key.get_pressed()
+
+    # Move the players
+    if keys[pygame.K_a]:
+        player1_x -= PLAYER_SPEED
+    if keys[pygame.K_d]:
+        player1_x += PLAYER_SPEED
+    if keys[pygame.K_w]:
+        if P1_Standing == True:
+            P1_Jumping = True
+            P1_Standing = False
+    
+    if keys[pygame.K_s]:
+        pass
+        # player1_y += PLAYER_SPEED
+
+    if keys[pygame.K_LEFT]:
+        player2_x -= PLAYER_SPEED
+    if keys[pygame.K_RIGHT]:
+        player2_x += PLAYER_SPEED
+    if keys[pygame.K_UP]:
+        if P2_Standing == True:
+            P2_Jumping = True
+            P2_Standing = False
+            
+    if keys[pygame.K_DOWN]:
+        pass
+        # player2_y += PLAYER_SPEED
+        
+
     ## "I want you to paint it, paint it, paint it black"
-    ## 0,0,0 is default black background
     screen.fill((0,0,0))
 
     ## Layering background image of map imagery
     screen.blit(BackGround.image, BackGround.rect)
+
+    font = pygame.font.SysFont('Algerian',75)
+    text = font.render("Art of War", 1,(255,255,255))
+
+    screen.blit(text, (750,50))
 
     if tick % 2 == 0:
         if P1_idle_frame < P1_idle_frameCount - 1:
@@ -208,10 +282,45 @@ while running:
 
     ## Spawn player sprites
     P1_link = f'{P1["Action"]["Idle"]["imagePath"]}\{P1_idle_frame}.png'
-    Player1 = GameSprite(P1_link, (350,500), (250,250), False)
+    Player1 = GameSprite(P1_link, (player1_x, player1_y), Default_Smoothscale_Dimensions, False)
 
     P2_link = f'{P2["Action"]["Idle"]["imagePath"]}\{P2_idle_frame}.png'
-    Player2 = GameSprite(P2_link, (1350,500), (250,250), True)
+    Player2 = GameSprite(P2_link, (player2_x, player2_y), Default_Smoothscale_Dimensions, True)
+
+
+    ## Jumping and Descending for Player 1
+    if P1_Jumping == True:
+        if P1_Descending == False:
+            player1_y -= VERTICAL_SPEED
+            P1_Jump_Height += VERTICAL_SPEED
+
+            if P1_Jump_Height == JUMP_HEIGHT:
+                P1_Descending = True
+        else:
+            player1_y += PLAYER_SPEED
+            P1_Jump_Height -= PLAYER_SPEED
+
+            if P1_Jump_Height == 0:
+                P1_Descending = False
+                P1_Jumping = False
+                P1_Standing = True
+
+    ## Jumping and Descending for Player 2
+    if P2_Jumping == True:
+        if P2_Descending == False:
+            player2_y -= VERTICAL_SPEED
+            P2_Jump_Height += VERTICAL_SPEED
+
+            if P2_Jump_Height == JUMP_HEIGHT:
+                P2_Descending = True
+        else:
+            player2_y += PLAYER_SPEED
+            P2_Jump_Height -= PLAYER_SPEED
+
+            if P2_Jump_Height == 0:
+                P2_Descending = False
+                P2_Jumping = False
+                P2_Standing = True
 
 
     tick += 1
