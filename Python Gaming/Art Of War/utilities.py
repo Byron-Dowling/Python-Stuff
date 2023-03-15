9f1c588184b3a96412b64760ffb6758a26e8b9d4
Leslie-N-Cook <leslienichole.co@gmail.com>
2023-03-14 21:26:02 -0500

    slash change

--- Python Gaming/Art Of War/utilities.py
+++ Python Gaming/Art Of War/utilities.py
@@ -3,7 +3,7 @@ import pygame
 def background_music():
     # pygame.mixer.init()
     #load the music from my files
-    pygame.mixer.music.load('battle_music/prepare-to-die-music.mp3')
+    pygame.mixer.music.load('battle_music\prepare-to-die-music.mp3')
     #set the volume so it doesnt blast anyones ear drums
     pygame.mixer.music.set_volume(.01)
     # tell it to play continuously
@@ -12,7 +12,7 @@ def background_music():
 def fight_sound_1():
     pygame.mixer.init()
     #load the music from my files
-    pygame.mixer.music.load('fight_sounds/sword-slide-fight.wav')
+    pygame.mixer.music.load('fight_sounds\sword-slide-fight.wav')
     #set the volume so it doesnt blast anyones ear drums
     pygame.mixer.music.set_volume(0.3)
     # tell it to play once
@@ -21,7 +21,7 @@ def fight_sound_1():
 def fight_sound_2():
     # pygame.mixer.init()
     #load the music from my files
-    pygame.mixer.music.load('fight_sounds/sword-cutt-flesh.wav')
+    pygame.mixer.music.load('fight_sounds\sword-cutt-flesh.wav')
     #set the volume so it doesnt blast anyones ear drums
     pygame.mixer.music.set_volume(0.3)
     # tell it to play once
@@ -30,7 +30,7 @@ def fight_sound_2():
 def fight_sound_3():
     # pygame.mixer.init()
     #load the music from my files
-    pygame.mixer.music.load('fight_sounds/knife-slice-cut.mp3')
+    pygame.mixer.music.load('fight_sounds\knife-slice-cut.mp3')
     #set the volume so it doesnt blast anyones ear drums
     pygame.mixer.music.set_volume(0.3)
     # tell it to play once
@@ -39,7 +39,7 @@ def fight_sound_3():
 def fight_sound_4():
     # pygame.mixer.init()
     #load the music from my files
-    pygame.mixer.music.load('fight_sounds/body-cut-impact.wav')
+    pygame.mixer.music.load('fight_sounds\body-cut-impact.wav')
     #set the volume so it doesnt blast anyones ear drums
     pygame.mixer.music.set_volume(0.3)
     # tell it to play once
@@ -48,7 +48,7 @@ def fight_sound_4():
 def win_sound():
     # pygame.mixer.init()
     #load the music from my files
-    pygame.mixer.music.load('fight_sounds/eren-titan-roar.mp3')
+    pygame.mixer.music.load('fight_sounds\eren-titan-roar.mp3')
     #set the volume so it doesnt blast anyones ear drums
     pygame.mixer.music.set_volume(0.1)
     # tell it to play once
